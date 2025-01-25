cf7081206766d5861473759dde2a81d7bee8fd46
Ismail Abdifadil <ismail.haabare@gmail.com>
2025-01-25 17:15:01 +0300

    Hero Size

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -6,45 +6,45 @@ import { motion } from 'framer-motion'
 const Hero = () => {
   return (
     <div
-      className='bg-cover bg-center h-screen'
+      className='bg-cover bg-center'
       style={{
         backgroundImage: `url(${heroPattern})`,
       }}
     >
-        <div className='container sm:px-6 lg:px-8 flex flex-col md:flex-row md:items-center justify-between py-32 md:py-10 lg:py-20 '>
-          {/* Left Content */}
-          <div className='md:w-1/2 lg:w-2/3'>
-            <h1 className='text-3xl sm:text-4xl md:text-5xl lg:text-6xl font-bold leading-snug md:leading-normal  md:text-left'>
-              Inspiring Youth to <br className='hidden md:block' />
-              Lead the Way in
-              <br className='hidden md:block' /> Building Stronger Communities
-            </h1>
+      <div className='container sm:px-6 lg:px-8 flex flex-col md:flex-row md:items-center justify-between py-32 md:py-10 lg:py-20 '>
+        {/* Left Content */}
+        <div className='md:w-1/2 lg:w-2/3'>
+          <h1 className='text-3xl sm:text-4xl md:text-5xl lg:text-6xl font-bold leading-snug md:leading-normal  md:text-left'>
+            Inspiring Youth to <br className='hidden md:block' />
+            Lead the Way in
+            <br className='hidden md:block' /> Building Stronger Communities
+          </h1>
 
-            <p className='text-brand-lightBody mt-4 mb-8 text-sm sm:text-base md:w-[450px] lg:w-[500px] mx-auto md:mx-0'>
-              Empowering the next generation to thrive through education,
-              advocacy, and community development.
-            </p>
-            <div className=' flex flex-col gap-3 md:flex-row'>
-              <Link to='/about-us'>
-                <Button className='w-full md:w-fit'>About Us</Button>
-              </Link>
-              <Link to='/programs'>
-                <Button variant='outline' className='w-full md:w-fit'>
-                  Our Programs
-                </Button>
-              </Link>
-            </div>
-          </div>
-
-          {/* Right Image */}
-          <div className='w-full md:w-1/2 lg:w-1/3'>
-            <img
-              src={heroImg}
-              className='hidden md:block w-[280px] sm:w-[320px] lg:w-[400px]'
-              alt='Communities'
-            />
+          <p className='text-brand-lightBody mt-4 mb-8 text-sm sm:text-base md:w-[450px] lg:w-[500px] mx-auto md:mx-0'>
+            Empowering the next generation to thrive through education,
+            advocacy, and community development.
+          </p>
+          <div className=' flex flex-col gap-3 md:flex-row'>
+            <Link to='/about-us'>
+              <Button className='w-full md:w-fit'>About Us</Button>
+            </Link>
+            <Link to='/programs'>
+              <Button variant='outline' className='w-full md:w-fit'>
+                Our Programs
+              </Button>
+            </Link>
           </div>
         </div>
+
+        {/* Right Image */}
+        <div className='w-full md:w-1/2 lg:w-1/3'>
+          <img
+            src={heroImg}
+            className='hidden md:block w-[280px] sm:w-[320px] lg:w-[400px]'
+            alt='Communities'
+          />
+        </div>
+      </div>
     </div>
   )
 }
